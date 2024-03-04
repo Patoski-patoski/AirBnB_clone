73c4f7c5b8a53353a23c1a0374e1005803164121
patrick okafor <patrickpeters911@gmail.com>
2024-03-04 08:49:25 -0800

    Update FileStorage to manage correctly serialization and deserialization of all our new classes:

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,10 +4,20 @@ import json
 from os.path import exists
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 my_objects = {
         "BaseModel": BaseModel,
         "User": User,
+        "Place": Place,
+        "State": State,
+        "City"; City,
+        "Amenity": Amenity,
+        "Review": Review,
 }
 
 
