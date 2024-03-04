71aa4eab8a41dda1b104302646d06af111dedaf3
patrick okafor <patrickpeters911@gmail.com>
2024-03-04 08:54:36 -0800

    Update FileStorage to manage correctly serialization and deserialization of all our new classes:

--- console.py
+++ console.py
@@ -4,6 +4,12 @@
 
 import cmd
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
 from models import storage
 from models.engine.file_storage import my_objects
 
