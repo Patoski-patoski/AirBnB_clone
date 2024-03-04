28f8d1da69119e131617491db4e2ce27e167cc09
patrick okafor <patrickpeters911@gmail.com>
2024-03-04 08:09:28 -0800

    a class User that inherits from BaseModel:

--- models/user.py
+++ models/user.py
@@ -5,7 +5,14 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """a class User that inherits from BaseModel"""
+    """a class User that inherits from BaseModel
+
+    Args:
+        email: string - empty string
+        password: string - empty string
+        first_name: string - empty string
+        last_name: string - empty string
+    """
     email = ""
     password = ""
     first_name = ""
