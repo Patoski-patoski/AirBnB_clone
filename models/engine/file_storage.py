778df6de1151e12370c48eeec538acb5ca2925d7
patrick okafor <patrickpeters911@gmail.com>
2024-03-07 09:16:11 -0800

    a class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,26 +30,28 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
         key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            j_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+        with open(self.__file_path, 'w') as f:
+            j_dict = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(j_dict, f)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
-        if exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
-                attr_dict = json.load(file)
-                for obj_id, value in attr_dict.items():
-                    obj = obj_id.split('.')[0]
-                    FileStorage.__objects[obj_id] = my_objects[obj](**value)
-        else:
+        try:
+            if exists(FileStorage.__file_path):
+                with open(self.__file_path, 'r', encoding='utf-8') as file:
+                    attr_dict = json.load(file)
+                    for obj_id, value in attr_dict.items():
+                        obj = obj_id.split('.')[0]
+                        self.__objects[obj_id] = my_objects[obj](**value)
+
+        except FileNotFoundError:
             pass
